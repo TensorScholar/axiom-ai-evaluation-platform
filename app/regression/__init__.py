300f4c3e97059011f48b6d110542574ed4aa4a3a
Mohammad Atashi <mohammadaliatashi@icloud.com>
2026-06-30 04:35:32 +0330

    Add regression suite records

--- app/regression/__init__.py
+++ app/regression/__init__.py
@@ -1,1 +1,15 @@
-"""Regression package placeholder."""
+from app.regression.models import RegressionCase, RegressionRerunPlan, RegressionSuite
+from app.regression.suite import (
+    build_regression_suite,
+    failure_to_regression_case,
+    plan_regression_rerun,
+)
+
+__all__ = [
+    "RegressionCase",
+    "RegressionRerunPlan",
+    "RegressionSuite",
+    "build_regression_suite",
+    "failure_to_regression_case",
+    "plan_regression_rerun",
+]
