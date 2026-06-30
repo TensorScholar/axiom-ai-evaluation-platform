a04021765f827d1f484a21208c5c1f60f58adf5d
Mohammad Atashi <mohammadaliatashi@icloud.com>
2026-06-30 12:40:32 +0330

    Add OpenAI provider adapter

--- app/providers/__init__.py
+++ app/providers/__init__.py
@@ -1,10 +1,13 @@
 from app.providers.base import ProviderAdapter
 from app.providers.fake import FakeProvider, NoScriptedResponseError
 from app.providers.models import ProviderRequest, ProviderResponse
+from app.providers.openai_provider import OpenAIProvider, OpenAIProviderError
 
 __all__ = [
     "FakeProvider",
     "NoScriptedResponseError",
+    "OpenAIProvider",
+    "OpenAIProviderError",
     "ProviderAdapter",
     "ProviderRequest",
     "ProviderResponse",
